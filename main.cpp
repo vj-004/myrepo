86d34b639e850d2947d9fd5677734253f5ba36a2
Vayun Jain <120264878+vj-004@users.noreply.github.com>
2023-12-16 12:04:11 +0530

    changed file type

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "bits/stdc++.h"
+
+
+void quicksort(){
+	std::cout<<"This is quicksort"<<'\n';
+}
+
+int main(){
+	std::cout<<"Hello World"<<'\n';
+	std::cout<<"Hello"<<'\n';
+	return 0;
+}
